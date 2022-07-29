a173c76fee70c9fc0dbc07f8e4ee195d2d792613
shakthika123 <110148454+shakthika123@users.noreply.github.com>
2022-07-29 11:53:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Simple PHP CRUD Script
+* Developed by TutorialsClass.com
+* URL:  http://tutorialsclass.com/code/php-simple-crud-application
+**/
+
+// Create database connection using config file
+include_once("config.php");
+
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
+<head>    
+    <title>Homepage</title>
+</head>
+
+<body>
+	<p style="background-image: url('ward.jpg');">
+<a href="add.php">Add New ward</a><br/><br/>
+
+    <table width='80%' border=1>
+
+    <tr>
+        <th>Ward Name</th> <th>Ward Number</th> <th>ward Tel:number</th> <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";    
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
+</html>
